02c1e79c0437cbdfa07604ea6dfff5233ef79614
Jlius Suweno <jliuss@live.com>
2020-11-24 22:11:06 +0700

    update

---
+++ Example/Pods/Target Support Files/JMain/JMain-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JMain : NSObject
+@end
+@implementation PodsDummy_JMain
+@end
---
+++ Example/Pods/Target Support Files/Pods-JMain_Tests/Pods-JMain_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JMain_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_JMain_Tests
+@end
